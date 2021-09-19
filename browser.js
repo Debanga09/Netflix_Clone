6846f5243346ffa8a1cc645d83dfa57d576b958a
Debanga09 <73235033+Debanga09@users.noreply.github.com>
2021-09-19 16:07:11 +0530

    Add files via upload

--- browser.js
+++ browser.js
@@ -1,2 +1,6 @@
 var music = document.getElementById('instrumental');
 music.volume=0.4;
+//var video= document.querySelector('play-btn').addEventListener('click',play_vid);
+function play_vid(){
+  alert("Page under Construct");
+}
